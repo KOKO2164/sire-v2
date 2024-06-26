99c5c635592755db6183f920a7d9ae784d25f11a
Eduarks <n00291389@upn.pe>
2024-06-26 14:35:03 -0500

    Update payment.blade.php

--- resources/views/compras/payment.blade.php
+++ resources/views/compras/payment.blade.php
@@ -20,25 +20,10 @@
                 </div>
                 <div class="space_pay">
                     <div class="red_important">
-                        <label class="mxy"><strong>IMPORTANTE: </strong>Verificar que su tarjeta esté habilitada para realizar compras online</label>
+                        <label class="mxy"><strong>IMPORTANTE: </strong>Verificar que su Yape tiene saldo</label>
                     </div>
                     <div class="campos_pay">
-                        <div class="nro_tarjeta">
-                            <label class="mxy">N° TARJETA</label>
-                            <input type="text" class="input_pay" placeholder="">
-                        </div>
-                        <div class="tit_tarjeta">
-                            <label class="mxy">TITULAR DE LA TARJETA</label>
-                            <input type="text" class="input_pay" pattern="[0-9]{13,19}" maxlength="19" placeholder="">
-                        </div>
-                        <div class="fecha_exp">
-                            <label class="mxy">FECHA DE EXPIRACIÓN</label>
-                            <input type="date" class="input_pay1" placeholder="">
-                        </div>
-                        <div class="cvc">
-                            <label class="mxy">CVC</label>
-                            <input type="text" class="input_pay" placeholder="">
-                        </div>
+                        <img class="qrkoko" src="{{asset('img/qr.jpg')}}" alt="">
                     </div>
                 </div>
             </div>
